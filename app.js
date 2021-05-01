b00cb68b736264e0c73bfdbb2d3142b8731faa20
Jerry <jerrsiphavong@icloud.com>
2021-05-01 13:21:03 -0400

    week 4

--- app.js
+++ app.js
@@ -2,19 +2,11 @@ var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var logger = require("morgan");
+const mongoose = require("mongoose");
 const passport = require("passport");
 const config = require("./config");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-const campsiteRouter = require("./routes/campsiteRouter");
-const promotionRouter = require("./routes/promotionRouter");
-const partnerRouter = require("./routes/partnerRouter");
-
-const mongoose = require("mongoose");
 const url = config.mongoUrl;
-
-// const url = "mongodb://localhost:27017/nucampsite";
 const connect = mongoose.connect(url, {
   useCreateIndex: true,
   useFindAndModify: false,
@@ -27,7 +19,33 @@ connect.then(
   (err) => console.log(err)
 );
 
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+const campsiteRouter = require("./routes/campsiteRouter");
+const promotionRouter = require("./routes/promotionRouter");
+const partnerRouter = require("./routes/partnerRouter");
+const uploadRouter = require("./routes/uploadRouter");
+const favoriteRouter = require("./routes/favoriteRouter");
+
 var app = express();
+
+// Secure traffic only
+app.all("*", (req, res, next) => {
+  //app.all catches any type of request coming into the server, use asterisk for wildcard request
+  if (req.secure) {
+    //.secure is set automatically to true
+    return next();
+  } else {
+    //if not secure, redirect to a secure link
+    console.log(
+      `Redirecting to: https://${req.hostname}:${app.get("secPort")}${req.url}`
+    );
+    res.redirect(
+      301,
+      `https://${req.hostname}:${app.get("secPort")}${req.url}`
+    );
+  }
+});
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -36,7 +54,7 @@ app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser("12345-67890-09876-54321"));
+//app.use(cookieParser('12345-67890-09876-54321')); //using signed cookies, so we provide the cookie parser with a secret key as an argument, can be anything
 
 app.use(passport.initialize());
 
@@ -48,6 +66,8 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/campsites", campsiteRouter);
 app.use("/promotions", promotionRouter);
 app.use("/partners", partnerRouter);
+app.use("/imageUpload", uploadRouter);
+app.use("/favorites", favoriteRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: "12345-67890-09876-54321",
   mongoUrl: "mongodb://localhost:27017/nucampsite",
+  facebook: {
+    clientId: "567928944600288",
+    clientSecret: "165932d799cff4fe098f857b110996e3",
+  },
 };
